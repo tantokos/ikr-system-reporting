f25e81e85fdd4dd940821b0a1bd17b3e83ffb850
tantokos <tanto.koswara@gmail.com>
2024-07-05 15:17:33 +0700

    fix 5 agein klik analisa precon

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -1356,15 +1356,15 @@ class ReportController extends Controller
             }
 
             if($request->detKategori == "result"){
-                $detAPKBranch=$detAPKBranch->where('result','=',$request->detResult);
+                $detAPKBranch=$detAPKBranch->whereRaw('result = ?' ,$request->detResult);
             }
 
             if($request->detKategori == "penagihan"){
-                $detAPKBranch=$detAPKBranch->where('result','=',$request->detResult)
+                $detAPKBranch=$detAPKBranch->whereRaw('result = ?' ,$request->detResult)
                                             ->where('penagihan','=',$request->detPenagihan);
             }
             if($request->detKategori == "root_couse"){
-                $detAPKBranch=$detAPKBranch->where('result','=',$request->detResult)
+                $detAPKBranch=$detAPKBranch->whereRaw('result = ?' ,$request->detResult)
                                             ->where('penagihan','=',$request->detPenagihan)
                                             ->where('root_couse','=',$request->detRoot_couse);
             }
